423175c9c4d0fabfcc0a225610d5f4e98b218598
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-08-21 22:40:55 -0400

    Set version to 1.0-pre-alpha
    Tags: version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 33
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0-pre-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
