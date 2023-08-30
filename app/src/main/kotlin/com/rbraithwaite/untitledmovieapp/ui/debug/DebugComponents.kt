35c5da4d8f6365f6bce8412ff3d27278c47ae4cb
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-08-29 21:08:50 -0400

    Import some debug utils from task app
    Tags: debug-tools

---
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/debug/DebugComponents.kt
@@ -0,0 +1,24 @@
+package com.rbraithwaite.untitledmovieapp.ui.debug
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+// REFACTOR [23-08-29 9:02p.m.] -- this should be extracted to a library.
+@Composable
+fun DebugPlaceholder(
+    label: String = "WIP",
+    color: Color? = null,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.background(color ?: randomColor())
+    ) {
+        Text(label)
+    }
+}
