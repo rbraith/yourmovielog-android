9354eea346801dd8764872e76173fdaa7271097b
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-09-03 16:45:02 -0400

    Create rating picker dialog stub
    Tags: feature

--- app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
@@ -1,12 +1,22 @@
 package com.rbraithwaite.untitledmovieapp.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.rbraithwaite.untitledmovieapp.ui.debug.DebugPlaceholder
 import com.rbraithwaite.untitledmovieapp.ui.debug.randomBackgroundColor
@@ -22,6 +32,15 @@ fun NewReviewScreen(
         null -> ""
     }
 
+    var shouldShowRatingDialog by remember { mutableStateOf(false) }
+
+    if (shouldShowRatingDialog) {
+        RatingPickerDialog(
+            onDismiss = { shouldShowRatingDialog = false },
+            onConfirm = { shouldShowRatingDialog = false }
+        )
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -31,8 +50,8 @@ fun NewReviewScreen(
         TextField(value = titleString, onValueChange = {})
 
         Text("rating")
-        Button(onClick = { /*TODO*/ }) {
-            Text("3.4 / 10")
+        Button(onClick = { shouldShowRatingDialog = true }) {
+            Text("-- / 10")
         }
 
         Text("date")
@@ -48,6 +67,46 @@ fun NewReviewScreen(
             Text("add context")
         }
     }
+}
+
+@Composable
+fun RatingPickerDialog(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit
+) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        text = {
+            Row {
+                TextField(
+                    value = "",
+                    onValueChange = {},
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Decimal
+                    ),
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    "/ 10",
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text("Confirm")
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun PreviewRatingPickerDialog() {
+    RatingPickerDialog(
+        onDismiss = {},
+        onConfirm = {}
+    )
 }
 
 @Preview
