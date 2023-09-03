4a3f75f61332a0465d51928ddcd52842c937267d
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-09-02 23:50:57 -0400

    Add some logging for quick search widget
    Tags: logging

--- app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/SearchScreen.kt
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/SearchScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rbraithwaite.untitledmovieapp.ui.debug.DebugPlaceholder
 import com.rbraithwaite.untitledmovieapp.ui.debug.randomBackgroundColor
+import timber.log.Timber
 
 @Composable
 fun SearchScreen() {
@@ -35,8 +36,14 @@ fun SearchScreen() {
             .randomBackgroundColor()
     ) {
         QuickSearchWidget(
-            onSearchInputChange = {},
-            onGoToAdvancedSearch = {},
+            onSearchInputChange = {
+                // TO IMPLEMENT
+                Timber.d("QuickSearchWidget onSearchInputChange = $it")
+            },
+            onGoToAdvancedSearch = {
+                // TO IMPLEMENT
+                Timber.d("QuickSearchWidget onSearchInputChange")
+            },
             modifier = Modifier.fillMaxWidth()
         )
 
