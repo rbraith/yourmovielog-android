5816ad87d0adea4d056c5a50c805090d931bee97
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-09-03 16:06:40 -0400

    Create rough stub of new review form
    Tags: feature

--- app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
@@ -1,10 +1,15 @@
 package com.rbraithwaite.untitledmovieapp.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rbraithwaite.untitledmovieapp.ui.debug.DebugPlaceholder
+import com.rbraithwaite.untitledmovieapp.ui.debug.randomBackgroundColor
 
 @Composable
 fun NewReviewScreen(
@@ -14,13 +19,35 @@ fun NewReviewScreen(
         is NewReviewSearchResult.CustomMedia -> {
             media.title
         }
-        null -> "no title"
+        null -> ""
     }
 
-    DebugPlaceholder(
-        label = titleString,
-        modifier = Modifier.fillMaxSize()
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .randomBackgroundColor()
+    ) {
+        Text("title")
+        TextField(value = titleString, onValueChange = {})
+
+        Text("rating")
+        Button(onClick = { /*TODO*/ }) {
+            Text("3.4 / 10")
+        }
+
+        Text("date")
+        Button(onClick = { /*TODO*/ }) {
+            Text("Today")
+        }
+
+        Text("review")
+        TextField(value = "", onValueChange = {})
+
+        Text("where did you watch this?")
+        Button(onClick = { /*TODO*/ }) {
+            Text("add context")
+        }
+    }
 }
 
 @Preview
