1a72ec9e3c09c8166ead426cce1009dfaef91962
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-09-05 23:56:21 -0400

    Extract RatingPickerDialog
    Tags: refactor

--- app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/NewReviewScreen.kt
@@ -76,49 +76,6 @@ fun NewReviewScreen(
     }
 }
 
-/**
- * @param onConfirm Returns selected rating as 0-100 Int, or null if rating is unselected
- */
-@Composable
-fun RatingPickerDialog(
-    initialRating: Int? = null,
-    onDismiss: () -> Unit,
-    onConfirm: (Int?) -> Unit
-) {
-    var ratingString by remember(initialRating) { mutableStateOf(formatRating(initialRating)) }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        text = {
-            Row {
-                TextField(
-                    value = ratingString,
-                    onValueChange = {
-                        if (isValidRatingString(it)) {
-                            ratingString = it
-                        }
-                    },
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Decimal
-                    ),
-                    modifier = Modifier.weight(1f)
-                )
-                Text(
-                    "/ 10",
-                    modifier = Modifier.weight(1f)
-                )
-            }
-        },
-        confirmButton = {
-            TextButton(onClick = {
-                onConfirm(parseRatingString(ratingString))
-            }) {
-                Text("Confirm")
-            }
-        }
-    )
-}
-
 private fun formatRating(rating: Int?): String {
     return if (rating == null) {
         ""
@@ -132,62 +89,6 @@ private fun formatRating(rating: Int?): String {
             "$beforeDecimal.$afterDecimal"
         }
     }
-}
-
-// the rating string arrives as a float value between 0-10, converted to int 0-100
-private fun parseRatingString(ratingString: String): Int? {
-    if (ratingString.isBlank()) {
-        return null
-    }
-
-    if (ratingString.endsWith(".")) {
-        return ratingString.dropLast(1).toIntOrNull()?.let { it * 10 }
-    }
-
-    if (ratingString.startsWith(".")) {
-        // return first digit after the decimal point
-        return ratingString.drop(1).getOrNull(0)?.digitToIntOrNull()
-    }
-
-    if (!ratingString.contains(".")) {
-        return ratingString.toIntOrNull()?.let { it * 10 }
-    }
-
-    // last case where there are digits on both sides of the decimal point
-    val decimalSplit = ratingString.split(".")
-    val beforeDec = decimalSplit.getOrNull(0)?.toIntOrNull()?.let { it * 10 }
-    // only first digit after decimal
-    val afterDec = decimalSplit.getOrNull(1)?.getOrNull(0)?.digitToIntOrNull()
-
-    if (beforeDec == null || afterDec == null) {
-        return null
-    }
-
-    return beforeDec + afterDec
-}
-
-private fun isValidRatingString(ratingString: String): Boolean  {
-    if (ratingString.matches(Regex("^10\\.?0?\$"))) {
-        // matches for 10, 10., 10.0
-        return true
-    }
-
-    if (ratingString.matches(Regex("^[0-9]?\\.?[0-9]?\$"))) {
-        // matches for values less than 10, such as .5, 8.2, etc
-        // limits to 1 decimal place
-        return true
-    }
-
-    return false
-}
-
-@Preview
-@Composable
-fun PreviewRatingPickerDialog() {
-    RatingPickerDialog(
-        onDismiss = {},
-        onConfirm = {}
-    )
 }
 
 @Preview
