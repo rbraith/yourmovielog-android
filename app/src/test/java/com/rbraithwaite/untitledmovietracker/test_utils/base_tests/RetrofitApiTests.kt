e09a77e188a8e1ee9c28e1580b3dd679a9ea46b5
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-12-20 15:00:41 -0500

    Remove IN PROGRESS comment oops
    Tags: comment

--- app/src/test/java/com/rbraithwaite/untitledmovietracker/test_utils/base_tests/RetrofitApiTests.kt
+++ app/src/test/java/com/rbraithwaite/untitledmovietracker/test_utils/base_tests/RetrofitApiTests.kt
@@ -4,7 +4,6 @@ import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
 import org.junit.Before
 
-// IN PROGRESS [23-12-2 12:43a.m.] this will be the base test class for api testing.
 abstract class RetrofitApiTests {
     lateinit var mockWebServer: MockWebServer
 
