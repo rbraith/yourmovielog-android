ed77fc5431e217f351ca288a1889142ac290a94b
Rich Braithwaite <rbraithwaite.dev@gmail.com>
2023-12-30 21:47:48 -0500

    Update bottom nav icons

--- app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/screen_main/MainBottomNavBar.kt
+++ app/src/main/kotlin/com/rbraithwaite/untitledmovieapp/ui/screen_main/MainBottomNavBar.kt
@@ -3,10 +3,14 @@ package com.rbraithwaite.untitledmovieapp.ui.screen_main
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MainBottomNavBar() {
@@ -15,15 +19,27 @@ fun MainBottomNavBar() {
             selected = true,
             onClick = { /*TODO*/ },
             icon = {
-                Icon(Icons.Filled.DateRange, contentDescription = "what")
+                Icon(Icons.Filled.Home, contentDescription = "what")
+            },
+            label = {
+                Text("home")
             }
         )
         NavigationBarItem(
             selected = false,
             onClick = { /*TODO*/ },
             icon = {
-                Icon(Icons.Filled.AccountCircle, contentDescription = "what")
+                Icon(Icons.Filled.Star, contentDescription = "what")
+            },
+            label = {
+                Text("reviews")
             }
         )
     }
+}
+
+@Preview
+@Composable
+fun PreviewMainBottomNavBar() {
+    MainBottomNavBar()
 }
